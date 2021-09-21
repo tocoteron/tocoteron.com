0b54ac102ee77b7aef4acc6cbd2661a33950e319
tocoteron <tocoteron@gmail.com>
2021-09-22 01:41:06 +0900

    Create tabs

--- frontend/src/components/templates/HomeTemplate.tsx
+++ frontend/src/components/templates/HomeTemplate.tsx
@@ -1,9 +1,22 @@
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import React from "react";
 import workExperiences from "../../data/workExperiences";
 import WorkExperiences from "../organisms/WorkExperiencesDisplay";
 
 const HomeTemplate: React.FC = () => (
-  <WorkExperiences workExperiences={workExperiences} />
+  <Tabs>
+    <TabList>
+      <Tab>Careers</Tab>
+      <Tab>Activities</Tab>
+      <Tab>Certifications</Tab>
+    </TabList>
+
+    <TabPanels>
+      <TabPanel>
+        <WorkExperiences workExperiences={workExperiences} />
+      </TabPanel>
+    </TabPanels>
+  </Tabs>
 );
 
 export default HomeTemplate;
