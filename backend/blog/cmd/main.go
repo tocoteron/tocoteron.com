80f7b7b58e9b950b5c7f4b8ad97a398d958650c3
tocoteron <tocoteron@gmail.com>
2021-10-09 18:28:07 +0900

    Use  environment variable for the port of blog backend

--- backend/blog/cmd/main.go
+++ backend/blog/cmd/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"tocoteron.com/blog/shared"
 )
 
 // For local test
 func main() {
-	log.Fatalln(shared.EchoServer.Start(":8080"))
+	portEnv := os.Getenv("PORT")
+	if portEnv == "" {
+		fmt.Println("Specify the port by $PORT environment variable")
+		os.Exit(1)
+	}
+
+	port, err := strconv.ParseUint(portEnv, 10, 16)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	log.Fatalln(shared.EchoServer.Start(fmt.Sprintf(":%d", port)))
 }
