2b3122e9feb4280b35a760ac4a307415184510dd
tocoteron <tocoteron@gmail.com>
2021-10-10 01:49:49 +0900

    Refactor

--- frontend/web/src/components/pages/BlogPage.tsx
+++ frontend/web/src/components/pages/BlogPage.tsx
@@ -1,44 +1,9 @@
 import React from "react";
 import BlogTemplate from "../templates/BlogTemplate";
-import BlogPost from "../../types/BlogPost";
-
-const posts: BlogPost[] = [
-  {
-    id: 0,
-    title: "Hello, world",
-    content: "# Hello\nThis is a first post.",
-    tags: ["test"],
-    createdAt: {
-      year: 2021,
-      month: 10,
-      day: 10,
-    },
-    updatedAt: {
-      year: 2021,
-      month: 10,
-      day: 10,
-    },
-  },
-  {
-    id: 1,
-    title: "Hello, world",
-    content: "# Hello\nThis is a first post.",
-    tags: ["test"],
-    createdAt: {
-      year: 2021,
-      month: 10,
-      day: 10,
-    },
-    updatedAt: {
-      year: 2021,
-      month: 10,
-      day: 10,
-    },
-  },
-];
+import blogPosts from "../../data/blogPosts";
 
 const BlogPage: React.FC = () => (
-  <BlogTemplate title="Tocoteron's Diary" posts={posts} />
+  <BlogTemplate title="Tocoteron's Diary" posts={blogPosts} />
 );
 
 export default BlogPage;
---
+++ frontend/web/src/data/blogPosts.ts
@@ -0,0 +1,38 @@
+import BlogPost from "../types/BlogPost";
+
+const posts: BlogPost[] = [
+  {
+    id: 0,
+    title: "Hello, world",
+    content: "# Hello\nThis is a first post.",
+    tags: ["test"],
+    createdAt: {
+      year: 2021,
+      month: 10,
+      day: 10,
+    },
+    updatedAt: {
+      year: 2021,
+      month: 10,
+      day: 10,
+    },
+  },
+  {
+    id: 1,
+    title: "Hello, world",
+    content: "# Hello\nThis is a first post.",
+    tags: ["test"],
+    createdAt: {
+      year: 2021,
+      month: 10,
+      day: 10,
+    },
+    updatedAt: {
+      year: 2021,
+      month: 10,
+      day: 10,
+    },
+  },
+];
+
+export default posts;
