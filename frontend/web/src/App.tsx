a7416e6528e86d15a6386e2842498e5e14cdd830
tocoteron <tocoteron@gmail.com>
2021-10-09 20:51:06 +0900

    Refactor

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { Box, ChakraProvider, Flex, HStack, Spacer } from "@chakra-ui/react";
-import Routes, { navigations } from "./routes";
-import NavigationDrawer from "./components/organisms/NavigationDrawer";
-import ColorModeToggleButton from "./components/atoms/ColorModeToggleButton";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import Routes from "./routes";
+import Header from "./components/templates/Header";
 import "./App.css";
 
 const App: React.FC = () => (
   <BrowserRouter>
     <ChakraProvider>
       <Box minW="100vw" minH="100vh">
-        <Flex as="header" w="full" px={4} py={4}>
-          <HStack>
-            <NavigationDrawer navigations={navigations} />
-            <ColorModeToggleButton />
-          </HStack>
-          <Spacer />
-        </Flex>
+        <Header />
         <Routes />
       </Box>
     </ChakraProvider>
---
+++ frontend/web/src/components/templates/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Flex, HStack, Spacer } from "@chakra-ui/react"
+import NavigationDrawer from "../organisms/NavigationDrawer"
+import ColorModeToggleButton from "../atoms/ColorModeToggleButton";
+import { navigations } from "../../routes"
+
+const Header: React.FC = () => (
+  <Flex as="header" w="full" px={4} py={4}>
+    <HStack>
+      <NavigationDrawer navigations={navigations} />
+      <ColorModeToggleButton />
+    </HStack>
+    <Spacer />
+  </Flex>
+)
+
+export default Header
